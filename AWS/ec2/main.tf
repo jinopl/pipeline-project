3d8b733da3afe2da36eec782fed50d2032adfb36
JINO <jinojp123@gmail.com>
2023-06-24 12:50:18 +0530

    fix app installtion - 3

--- AWS/ec2/main.tf
+++ AWS/ec2/main.tf
@@ -26,15 +26,14 @@ resource "aws_instance" "aws-pipeline" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.generated_key.key_name
   vpc_security_group_ids = [aws_security_group.aws-pipeline-ingress-sg.id , aws_security_group.aws-pipeline-egress-sg.id]
-  user_data     = <<EOF
-    #!/bin/bash -xe
-    sudo apt update
-    sudo apt upgrade -y
-    sudo apt-get -qq -y install apache2
-    sudo systemctl start apache2
-    curl https://gist.githubusercontent.com/jinopl/48fff7865190b7cd0a86a8127a5d29bd/raw/1c3b21248733dfdc96ee3035233f143024a298bb/hostname.html > ~/index.html
-    sudo mv ~/index.html /var/www/html/index.html
-    EOF
+  user_data = << EOF
+		#! /bin/bash
+    sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
   tags = {
     Name = "aws-pipeline"
   }
