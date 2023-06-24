c000867b41baa771e9f3fa6a027b0fcf86a852b9
JINO <jinojp123@gmail.com>
2023-06-24 17:38:12 +0530

    fix app installation

--- AWS/ec2/main.tf
+++ AWS/ec2/main.tf
@@ -32,7 +32,7 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl start apache2 
 sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>AWS pipeline-project</h1>" | sudo tee /var/www/html/index.html
 EOF
   tags = {
     Name = "aws-pipeline"
